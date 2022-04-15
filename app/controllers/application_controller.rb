145bce4f5ba4eb6471f45b5b566184da29650f58
nagakihidenori <100837116+nagakihidenori@users.noreply.github.com>
2022-04-15 18:29:46 +0900

    ユーザー管理の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+before_action :make_address_hash
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+private
+
+def make_address_hash
+  @address = Address.all
+end
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :address_id])
+end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :address
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :address, presence: true
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX
+         
+end
